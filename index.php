4a3a8f32c388a852d59c30e794381b8ac3091d51
matsnahr <matsnahr01@gmail.com>
2020-09-24 11:45:52 +0700

    update

--- index.php
+++ index.php
@@ -2,148 +2,134 @@
 
 function hello()
 {
-    echo "Hello, woeld";
+    echo "hello, world!";
 }
 
-hello(); //Hello, World
+hello();
+echo PHP_EOL;
+// ==============================
+$current_user="mike";
 
-// function is mike()
+function is_mike()
 {
+    global $current_user;
+
+
     if($current_user=="mike")
     {
-    echo "You is mike";
+        echo "you is mike \n";
     }else
     {
-    echo "Nope, you not Mike";
+        echo "nope, you not mike";
     }
 }
-
-
-function helloNne($name)
+is_mike();
+// ===============================
+function helloname($name)
+    
 {
-    if(i_array($name) && !empety($name))
+    if(is_array($name) && !empty($name))
     {
-        foreach($nama as $key)
-        {
-            echo "Hello, $key /n";
-        }
+     foreach($name as $key)
+     {
+        echo "hello, $key \n";
+     }
     }else
-    {
-        echo "Hello Friend !";
-    }
+  {
+    echo "hello friend!";
+  }
 }
 
-halloName(["Mike","Mohede","Bromton"]);
+helloname(["mike","kusuma","yuda"]);
+// ===========================
+echo PHP_EOL;
 
-var_dump(empty("0"));
-
-
-function get_info($name,$title=Null)
+function get_info($name,$title=null)
 {
     if($title)
     {
-        echo "$name was arrived,and came as $title";
+        echo "$name was arrived, and came as $title";
     }else
     {
-        echo "$name was arrived,welcome";
+        echo "$name was arrived, welcome";
     }
 }
-
 get_info("mike","frog");
 
-function hello_world()
+echo PHP_EOL;
+// =====================================
+function hello_world($word)
 {
-  return "\nHello, World";  
+    return "\n hello word $word";
 }
-
-$hello=helloWorld("are you ready to lose ?");
-
+$hello=hello_world($word="halo");
 echo $hello;
-
-// ehco PHP_EOL;
-
-function add_up($a,$b)
+// ====================================
+echo PHP_EOL;
+function addup($a,$b)
 {
-    return [$a,$b,$a+$b];
+    return [$a,$b,$a+$b];//array
 }
+print_r(addup(2,4));
+// =================================
+echo PHP_EOL;
 
-print_r(addUp(2,4));
-
-function sumArray($data)
+function sumarray($data)
 {
     $result=0;
     foreach($data as $key)
     {
         $result+=$key;
     }
-
     return $result;
 }
-
-echo (sumArray([1,4,5,5]));
-
-function answer()
-{
-    return "You answer the question";
-}
-
-$answer=answer();
-
-echo  PHP_EOL;
-
-// callback Function
-
-$answer_cal="answer";
-
-echo $answer_cal()."!!"; //call function
-
-// Anonymous Function
-
-$askName="What is your name ?";
-
+var_dump(sumarray([1,4,5,5]));
+// ================================
 echo PHP_EOL;
-
-$ask=function()use($askName,$name)
+function  answer()
 {
-    return "Ask the question, $askName $nama";
+    echo "you answer the question";
+}
+$answer=answer();
+echo PHP_EOL;
+$answer_cal="answer";
+echo $answer_cal()."!!"; //call the function
+// ===============================
+//anonymous function
+$askname="what is your name?";
+$name="budi";
+echo PHP_EOL;
+$ask=function()use($askname,$name)
+{
+    return "ask the question, $askname $name?";
 };
-
 echo $ask();
+echo PHP_EOL;
+// ==================================
+// built-in string function
+$magang="magang top jos";
+echo strlen($magang);
+echo substr($magang,7);//top jos
+echo PHP_EOL;
+echo substr($magang,7,6);//top jo
+echo PHP_EOL;
+echo strpos($magang,"jos");//11
+echo PHP_EOL;
+echo $magang[11];//j
+echo PHP_EOL;
+// ==================================
+$team=["mike"=>"developer","chris"=>"designer","hompton"=>"manager"];
+print_r(array_keys($team));
+var_dump(array_key_exists("john",$team));
 
-// Build-in string function
-
-$magang="Magang TOP";
-
-echo strlen($magang); //14
-
-br();
-
-echo substr($magang,7); //TOP jos
-
-br();
-
-echo substr($magang,7,6); //TOP jo
-
-br();
-
-echo strpos($magang,"j"); //11
-
-br();
-
-echo $magang[11]; //j
-
-$team=["Mike"=>"Developer","Chirs"=>"Designer","Brompton"=>"Manager"];
-
-print_r(array_key_exists("john",$team));
-
+if(array_key_exists("chris",$team))
+{
+    echo "mike found in \$team";
+}else
+{
+    echo "key not found in \$team";
+}
+echo PHP_EOL;
 array_walk($team,function($value,$key){
-
-    echo " $key is a $value \n";
+    echo "$key is a $value \n";
 });
-
-
-
-
-print_r
-
-?>
